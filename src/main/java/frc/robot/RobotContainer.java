cb75ea86a503438719df6e5b6e1c3c47492e86d1
RileyTurtle123 <rauchfam1@gmail.com>
2020-12-15 20:01:11 -0600

    Basic Intake Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,20 +9,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.StopIntake;
+import frc.robot.subsystems.Intake;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
- */
+
+
 public class RobotContainer {
+  //Subsystem
+  private final Intake mIntake;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    //Intake
+    mIntake = new Intake();
+    mIntake.setDefaultCommand(new StopIntake(mIntake));
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -34,6 +37,7 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+
 
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  //Intake Motor
+  private CANSparkMax intakeMotor;
+
+  //Intake Solenod
+  private Solenoid intakeDeploySol;
+
+  /**
+   * Creates a new Intake.
+   */
+  public Intake() {
+    //Intake Motor 
+    intakeMotor = new CANSparkMax(16, MotorType.kBrushless);
+    intakeMotor.setInverted(false);
+    intakeMotor.setIdleMode(IdleMode.kCoast);
+
+    //Intake Solenoid
+    intakeDeploySol = new Solenoid(1);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void setIntakeSpeed(double Speed) {
+    // Intake Speed
+    intakeMotor.set(Speed);
+
+  }
+  public void deployIntake(Boolean toggle) {
+    //Intake Deploy
+    intakeDeploySol.set(toggle);
+    
+  }
+}
