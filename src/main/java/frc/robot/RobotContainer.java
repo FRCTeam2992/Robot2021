5a4ec68b480c19043794d252d4bc8c3a01815d26
RileyTurtle123 <rauchfam1@gmail.com>
2020-12-17 20:16:44 -0600

    Turret and ClimeSlide

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,23 +7,35 @@
 
 package frc.robot;
 
+import frc.robot.commands.StopClimeSlide;
 import frc.robot.commands.StopIntake;
+import frc.robot.subsystems.ClimeSlide;
 import frc.robot.subsystems.Intake;
 import frc.robot.commands.StopSpindexer;
+import frc.robot.commands.StopTurret;
 import frc.robot.subsystems.Spindexer;
+import frc.robot.subsystems.Turret;
 
 public class RobotContainer {
-  // Subsystem
+  // Subsystems
   private final Intake mIntake;
-
   private final Spindexer mSpindexer;
+  private final Turret mTurret;
+  private final ClimeSlide mClimeSlide;
 
   public RobotContainer() {
     // Intake
     mIntake = new Intake();
     mIntake.setDefaultCommand(new StopIntake(mIntake));
 
-    // Configure the button bindings
+    //Turret
+    mTurret = new Turret();
+    mTurret.setDefaultCommand(new StopTurret(mTurret));
+
+    //ClimeSlide
+    mClimeSlide = new ClimeSlide();
+    mClimeSlide.setDefaultCommand(new StopClimeSlide(mClimeSlide));
+    
     mSpindexer = new Spindexer();
     mSpindexer.setDefaultCommand(new StopSpindexer(mSpindexer));
     configureButtonBindings();
---
+++ src/main/java/frc/robot/subsystems/ClimeSlide.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimeSlide extends SubsystemBase {
+
+  private VictorSPX climeMotor;
+  /**
+   * Creates a new ClimeSlide.
+   */
+  public ClimeSlide() {
+    climeMotor = new VictorSPX(12);
+    climeMotor.setInverted(false);
+    climeMotor.setNeutralMode(NeutralMode.Coast);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void SetClimeSpeed(double speed) {
+
+    climeMotor.set(ControlMode.PercentOutput, speed);
+
+  }
+}
