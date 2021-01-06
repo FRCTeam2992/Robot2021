70b48acd3c05d48d73caadbc60818232e4845b9a
RileyTurtle123 <rauchfam1@gmail.com>
2021-01-05 20:21:42 -0600

    Color Wheel Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,16 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorMatch;
+
+import edu.wpi.first.wpilibj.util.Color;
+
 public final class Constants {
 
+    
+// Color Wheel Target Colors
+public static final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+public static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+public static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+public static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 }
