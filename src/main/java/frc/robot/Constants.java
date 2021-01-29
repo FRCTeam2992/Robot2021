cc5940f9c494e55b9875aa995cff59661af0c7a2
Reid-Jourdan <reid.jourdan@gmail.com>
2021-01-28 19:05:59 -0600

    did stuff for riley

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,25 +9,88 @@ package frc.robot;
 
 import com.revrobotics.ColorMatch;
 
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.util.Color;
 
 public final class Constants {
-    //teleclimb
+    // teleclimb
     public static final int teleClimbLimit = 4000;
 
-    //shooter
+    // shooter
     public static final int defaultShooterSpeed = 5200;
     public static final int shooterEncoderPulses = 2048;
-    
-// Color Wheel Target Colors
-public static final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-public static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-public static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-public static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-public static final int colorWheelEncoderPulses = 1024;
-public static final double colorWheelSpinRatio = 8.0;
-public static final double colorSensorMinAngle = 0.0;
-public static final double colorSensorMaxAngle = 180.0;
-public static final double colorWheelSpinRotations = 4.0;
+    // Color Wheel Target Colors
+    public static final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    public static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    public static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    public static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+    public static final int colorWheelEncoderPulses = 1024;
+    public static final double colorWheelSpinRatio = 8.0;
+    public static final double colorSensorMinAngle = 0.0;
+    public static final double colorSensorMaxAngle = 180.0;
+    public static final double colorWheelSpinRotations = 4.0;
+
+    // Drive Variables
+    public static boolean isFieldCentric = true;
+    public static boolean isVelocityControlled = true;
+
+    // Length and Width of the Robot in Meters (Inches: 24.5 x 22.0)
+    public static final double swerveWidth = 0.6223;
+    public static final double swerveLength = 0.5588;
+
+    // Max Swerve Speed (Velocity Control)
+    public static final double swerveMaxSpeed = 3.5; // (Meters per Second)
+
+    // Max Path Following Drive Speeds
+    public static final double maxPathFollowingVelocity = 3.5; // (Meters per Second)
+    public static final double maxPathFollowingAcceleration = 1.5; // (Meters per Second Squared)
+
+    // Max Path Following Turn Speeds
+    public static final double maxThetaVelocity = 3.14; // (Radians per Second)
+    public static final double maxThetaAcceleration = 1.50; // (Radians per Second Squared)
+
+    // Swerve Module Translations
+    public static final Translation2d frontLeftLocation = new Translation2d(0.2794, 0.31115);
+    public static final Translation2d frontRightLocation = new Translation2d(0.2794, -0.31115);
+    public static final Translation2d rearLeftLocation = new Translation2d(-0.2794, 0.31115);
+    public static final Translation2d rearRightLocation = new Translation2d(-0.2794, -0.31115);
+
+    // Swerve Wheels and Gear Ratio
+    public static final double driveGearRatio = 8.33;
+    public static final double driveWheelDiameter = 0.1016;
+
+    // Analog Encoder Offsets (Degrees) - Opposite of Reading
+    public static final double frontLeftOffset = -111.7;
+    public static final double frontRightOffset = 0.8;
+    public static final double rearLeftOffset = 98.0;
+    public static final double rearRightOffset = 94.8;
+
+    // Swerve Drive PID (Velocity Control)
+    public static final double driveP = 0.00025;
+    public static final double driveI = 0.0;
+    public static final double driveD = 0.0;
+    public static final double driveF = 0.00018;
+
+    // Swerve Turn PID
+    public static final double turnP = 0.005;
+    public static final double turnI = 0.0;
+    public static final double turnD = 0.0;
+
+    // Swerve X Axis Correction PID (Path Following)
+    public static final double xCorrectionP = 1.0;
+    public static final double xCorrectionI = 0.0;
+    public static final double xCorrectionD = 0.0;
+
+    // Swerve Y Axis Correction PID (Path Following)
+    public static final double yCorrectionP = 1.0;
+    public static final double yCorrectionI = 0.0;
+    public static final double yCorrectionD = 0.0;
+
+    // Swerve Theta Axis Correction PID (Path Following)
+    public static final double thetaCorrectionP = 1.0;
+    public static final double thetaCorrectionI = 0.0;
+    public static final double thetaCorrectionD = 0.0;
+
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,7 +4,11 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.controller.PIDController;
@@ -13,6 +17,7 @@ import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.drive.swerve.SwerveModule;
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
 
@@ -62,6 +67,100 @@ public class DriveTrain extends SubsystemBase {
 
   /** Creates a new DriveTrain. */
   public DriveTrain() {
+    //Drive motors
+    //Front Left
+    frontLeftDrive = new CANSparkMax(1, MotorType.kBrushless);
+    frontLeftDrive.setInverted(false);
+    frontLeftDrive.setIdleMode(IdleMode.kCoast);
+    frontLeftDrive.setSmartCurrentLimit(30);
+    frontLeftDrive.setOpenLoopRampRate(0.2);
+
+    frontLeftTurn = new CANSparkMax(2, MotorType.kBrushless);
+    frontLeftTurn.setInverted(false);
+    frontLeftTurn.setIdleMode(IdleMode.kCoast);
+    frontLeftTurn.setSmartCurrentLimit(30);
+
+    //Front Right
+    frontRightDrive = new CANSparkMax(3, MotorType.kBrushless);
+    frontRightDrive.setInverted(false);
+    frontRightDrive.setIdleMode(IdleMode.kCoast);
+    frontRightDrive.setSmartCurrentLimit(30);
+    frontRightDrive.setOpenLoopRampRate(0.2);
+
+    frontRightTurn = new CANSparkMax(4, MotorType.kBrushless);
+    frontRightTurn.setInverted(false);
+    frontRightTurn.setIdleMode(IdleMode.kCoast);
+    frontRightTurn.setSmartCurrentLimit(30);
+
+    //Rear Left
+    rearLeftDrive = new CANSparkMax(5, MotorType.kBrushless);
+    rearLeftDrive.setInverted(false);
+    rearLeftDrive.setIdleMode(IdleMode.kCoast);
+    rearLeftDrive.setSmartCurrentLimit(30);
+    rearLeftDrive.setOpenLoopRampRate(0.2);
+
+    rearLeftTurn = new CANSparkMax(6, MotorType.kBrushless);
+    rearLeftTurn.setInverted(false);
+    rearLeftTurn.setIdleMode(IdleMode.kCoast);
+    rearLeftTurn.setSmartCurrentLimit(30);
+
+    //Rear Right
+    rearRightDrive = new CANSparkMax(7, MotorType.kBrushless);
+    rearRightDrive.setInverted(false);
+    rearRightDrive.setIdleMode(IdleMode.kCoast);
+    rearRightDrive.setSmartCurrentLimit(30);
+    rearRightDrive.setOpenLoopRampRate(0.2);
+  
+    rearRightTurn = new CANSparkMax(4, MotorType.kBrushless);
+    rearRightTurn.setInverted(false);
+    rearRightTurn.setIdleMode(IdleMode.kCoast);
+    rearRightTurn.setSmartCurrentLimit(30);
+
+    //Drive Encoders
+    frontLeftEncoder = new AnalogInput(0);
+    frontRightEncoder = new AnalogInput(1);
+    rearLeftEncoder = new AnalogInput(2);
+    rearRightEncoder = new AnalogInput(3);
+
+    //Turn PID controller
+    frontLeftController = new PIDController(Constants.turnP, Constants.turnI, Constants.turnD);
+    frontLeftController.enableContinuousInput(-180.0, 180.0);
+
+    frontRightController = new PIDController(Constants.turnP, Constants.turnI, Constants.turnD);
+    frontRightController.enableContinuousInput(-180.0, 180.0);
+
+    rearLeftController = new PIDController(Constants.turnP, Constants.turnI, Constants.turnD);
+    rearLeftController.enableContinuousInput(-180.0, 180.0);
+
+    rearRightController = new PIDController(Constants.turnP, Constants.turnI, Constants.turnD);
+    rearRightController.enableContinuousInput(-180.0, 180.0);
+
+    //Set drive pid controllers
+    CANPIDController frontLeftDrivePID = frontLeftDrive.getPIDController();
+    frontLeftDrivePID.setP(Constants.driveP);
+    frontLeftDrivePID.setI(Constants.driveI);
+    frontLeftDrivePID.setD(Constants.driveD);
+    frontLeftDrivePID.setFF(Constants.driveF);
+
+    CANPIDController frontRightDrivePID = frontRightDrive.getPIDController();
+    frontRightDrivePID.setP(Constants.driveP);
+    frontRightDrivePID.setI(Constants.driveI);
+    frontRightDrivePID.setD(Constants.driveD);
+    frontRightDrivePID.setFF(Constants.driveF);
+
+    CANPIDController rearLeftDrivePID = rearLeftDrive.getPIDController();
+    rearLeftDrivePID.setP(Constants.driveP);
+    rearLeftDrivePID.setI(Constants.driveI);
+    rearLeftDrivePID.setD(Constants.driveD);
+    rearLeftDrivePID.setFF(Constants.driveF);
+
+    CANPIDController rearRightDrivePID = rearRightDrive.getPIDController();
+    rearRightDrivePID.setP(Constants.driveP);
+    rearRightDrivePID.setI(Constants.driveI);
+    rearRightDrivePID.setD(Constants.driveD);
+    rearRightDrivePID.setFF(Constants.driveF);
+
+    //
 
   }
 
