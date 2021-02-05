ba0c67f99cdf4df7738180d18e58ce90369e44f2
RileyTurtle123 <rauchfam1@gmail.com>
2021-02-04 19:47:00 -0600

    AutoFollowPath Code

---
+++ src/main/java/frc/robot/commands/AutoFollowPath.java
@@ -0,0 +1,114 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import javax.xml.crypto.dsig.Transform;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.HolonomicDriveController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Transform2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.lib.drive.swerve.SwerveModule;
+import frc.lib.drive.swerve.trajectory.SwerveTrajectory;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+import jdk.vm.ci.meta.Constant;
+
+public class AutoFollowPath extends CommandBase {
+
+  // Subsystem Instance
+  private final DriveTrain mDriveTrain;
+
+  // Trajectory Instance
+  private final SwerveTrajectory mSwerveyTrajectory;
+  private final Trajectory mTrajectory;
+
+  // Drive Controller Instance
+  private final HolonomicDriveController controller;
+
+  // Variables
+  private final Timer elapsedTimer;
+
+  /** Creates a new AutoFollowPath. */
+  public AutoFollowPath(DriveTrain subsystem, SwerveTrajectory swerveTrajectory) {
+    // Save the Subsystem Instance
+    mDriveTrain = subsystem;
+
+    // Save the Swerve Trajectory
+    mSwerveyTrajectory = swerveTrajectory;
+
+    // Get the Trajectory
+    mTrajectory = mSwerveyTrajectory.getTrajectory();
+
+    // set the Subsystem Requirement
+    addRequirements(mDriveTrain);
+
+    // Initialize the Drive Controller
+    controller = new HolonomicDriveController(
+      new PIDController(Constants.xCorrectionP, Constants.xCorrectionI, Constants.xCorrectionD),
+      new PIDController(Constants.yCorrectionP, Constants.yCorrectionI, Constants.yCorrectionD),
+      new ProfiledPIDController(Constants.thetaCorrectionP, Constants.thetaCorrectionI, Constants.thetaCorrectionD,
+      new TrapezoidProfile.Constraints(Constants.maxThetaVelocity, Constants.maxThetaAcceleration)));
+
+      // Initialize the Path Timer
+      elapsedTimer = new Timer();
+    }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // Set the Trajectory Start Position to the Current Robot Position
+    Transform2d transform = mDriveTrain.latestSwervePose.minus(mTrajectory.getInitialPose());
+    mTrajectory.transformBy(transform);
+
+    // Reset and Start Elapsed Timer
+    elapsedTimer.reset();
+    elapsedTimer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Get the Current Time
+    double currentTime = elapsedTimer.get();
+
+    // Get the Latest State
+    Trajectory.State latestState = mTrajectory.sample(currentTime);
+
+    // Get the Desired Heading
+    double heading = mSwerveyTrajectory.getDesiredHeading(currentTime);
+
+    //Get the Ajusted Speeds
+    ChassisSpeeds adjustSpeeds = controller.calculate(mDriveTrain.latestSwervePose, latestState, Rotation2d.fromDegrees(heading));
+
+    // Get the Module States
+    SwerveModuleState[] moduleStates = mDriveTrain.swerveDriveKinematics.toSwerveModuleStates(adjustSpeeds);
+
+    // Set the Module States
+    mDriveTrain.frontLeftModule.setState(moduleStates[0]);
+    mDriveTrain.frontRightModule.setState(moduleStates[1]);
+    mDriveTrain.rearLeftModule.setState(moduleStates[2]);
+    mDriveTrain.rearRightModule.setState(moduleStates[3]);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    mDriveTrain.stopDrive();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return elapsedTimer.get() > mTrajectory.getTotalTimeSeconds();
+  }
+}
