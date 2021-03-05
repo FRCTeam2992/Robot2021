c699c68ddefa1ffca7cdb8ca504c9c3f74e002db
Galactic3D <matthewschaff2@gmail.com>
2021-03-04 19:22:49 -0600

    Formatted RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,15 +20,15 @@ public class RobotContainer {
   public final DriveTrain mDriveTrain;
   private final Intake mIntake;
   private final Spindexer mSpindexer;
-  private final Shooter mShooter;
-  // private final Ejector mEjector;
-  private final AdjustabeShooterHood mAdjustabeShooterHood;
 
   // Subsytem Instances (Disabled)
   // private final Turret mTurret;
   // private final ClimbSlide mClimeSlide;
   // private final TelescopeClimb mTelescopeClimb;
   // private final ColorWheel mColorWheel;
+  // private final Shooter mShooter;
+  // private final Ejector mEjector;
+  // private final AdjustabeShooterHood mAdjustabeShooterHood;
 
   // Controllers
   public mhController controller;
@@ -45,12 +45,6 @@ public class RobotContainer {
     mSpindexer = new Spindexer();
     mSpindexer.setDefaultCommand(new StopSpindexer(mSpindexer));
 
-    mShooter = new Shooter();
-    mShooter.setDefaultCommand(new StopShooter(mShooter));
-
-    // mEjector = new Ejector();
-    // mEjector.setDefaultCommand(new StopEjector(mEjector));
-
     // Subsystem Instances (Disabled)
     // mTurret = new Turret();
     // mTurret.setDefaultCommand(new StopTurret(mTurret));
@@ -64,33 +58,22 @@ public class RobotContainer {
     // mTelescopeClimb = new TelescopeClimb();
     // mTelescopeClimb.setDefaultCommand(new StopTelescopeClimb(mTelescopeClimb));
 
-    mAdjustabeShooterHood = new AdjustabeShooterHood();
-    mAdjustabeShooterHood.setDefaultCommand(new StopAdjustableShooterHood(mAdjustabeShooterHood));
+    // mAdjustabeShooterHood = new AdjustabeShooterHood();
+    // mAdjustabeShooterHood.setDefaultCommand(new
+    // StopAdjustableShooterHood(mAdjustabeShooterHood));
 
-    SmartDashboard.putData("Module Angles -90", new SetSwerveAngle(mDriveTrain, -90.0));
-    SmartDashboard.putData("Module Angles 90", new SetSwerveAngle(mDriveTrain, 90.0));
-    SmartDashboard.putData("Module Angles 0", new SetSwerveAngle(mDriveTrain, 0.0));
+    // mShooter = new Shooter();
+    // mShooter.setDefaultCommand(new StopShooter(mShooter));
+
+    // mEjector = new Ejector();
+    // mEjector.setDefaultCommand(new StopEjector(mEjector));
+
     // SmartDashboard Auto Paths
     SmartDashboard.putData("Straight Path",
         new AutoFollowPath(mDriveTrain, new StraightPath().generateSwerveTrajectory()));
 
     SmartDashboard.putData("Slalom Path",
         new AutoFollowPath(mDriveTrain, new SlalomPath(mDriveTrain).generateSwerveTrajectory()));
-
-    SmartDashboard.putData("Intake 100%", new MoveIntake(mIntake, 1.0));
-    SmartDashboard.putData("Intake -100%", new MoveIntake(mIntake, -1.0));
-
-    SmartDashboard.putData("Spindexer 50%", new MoveSpindexer(mSpindexer, 0.5));
-    SmartDashboard.putData("Spindexer -50%", new MoveSpindexer(mSpindexer, -0.5));
-    
-    SmartDashboard.putData("Intake Solenoid True", new DeployIntake(mIntake, true));
-    SmartDashboard.putData("Intake Solenoid False", new DeployIntake(mIntake, false));
-
-    SmartDashboard.putData("Spin Shooter 50%", new SetShooterSpeed(mShooter, 0.5));
-    SmartDashboard.putData("Spin Shooter -50%", new SetShooterSpeed(mShooter, -0.5));
-
-    SmartDashboard.putData("Adjustable shooter hood 25%", new MoveAdjustableShooterHood(mAdjustabeShooterHood, .25));
-    SmartDashboard.putData("Adjustable shooter hood -25%", new MoveAdjustableShooterHood(mAdjustabeShooterHood, -.25));
 
     // Initialize the Controller
     controller = new mhController(0);
