b78d229bf2c92b098d2fb56976b8723023f334bf
Galactic3D <matthewschaff2@gmail.com>
2021-04-27 18:34:14 -0500

    Move Light Code to Separate Files

---
+++ Light Code/Reid/Reid.ino
@@ -0,0 +1,39 @@
+#include <Adafruit_NeoPixel.h>
+
+// Spindexer Lights
+#define SPINDEXER_PIN 6
+#define SPINDEXER_COUNT 72
+#define SPINDEXER_REAL_COUNT 145
+
+// Spindexer Lights
+Adafruit_NeoPixel spindexerStrip = Adafruit_NeoPixel(SPINDEXER_REAL_COUNT, SPINDEXER_PIN, NEO_GRB + NEO_KHZ800);
+
+// Color Codes
+uint32_t red = spindexerStrip.Color(255, 0, 0);
+uint32_t orange = spindexerStrip.Color(255, 50, 0);
+uint32_t yellow = spindexerStrip.Color(255, 160, 0);
+uint32_t green = spindexerStrip.Color(0, 255, 0);
+uint32_t blue = spindexerStrip.Color(0, 0, 255);
+uint32_t purple = spindexerStrip.Color(255, 0, 255);
+
+void setup() {
+  // Spindexer Lights
+  BackgroundColor();
+  spindexerStrip.setBrightness(255);
+  spindexerStrip.begin();
+  spindexerStrip.show();
+}
+
+void loop() {
+
+}
+
+void setSpindexerPixel(int index, int r, int g, int b) {
+  spindexerStrip.setPixelColor(index, r, g, b);
+  spindexerStrip.setPixelColor((SPINDEXER_REAL_COUNT - 1) - index, r, g, b);
+}
+
+void setSpindexerPixel(int index, uint32_t color) {
+  spindexerStrip.setPixelColor(index, color);
+  spindexerStrip.setPixelColor((SPINDEXER_REAL_COUNT - 1) - index, color);
+}
