bf6cb62d1a31c84470f9a6e43c18f225bb8caffe
Reid-Jourdan <reid.jourdan@gmail.com>
2021-10-20 17:48:08 -0500

    add braces to an "if statement" and reformat

--- src/main/java/frc/robot/subsystems/TelescopeClimb.java
+++ src/main/java/frc/robot/subsystems/TelescopeClimb.java
@@ -35,27 +35,26 @@ public class TelescopeClimb extends SubsystemBase {
 
   @Override
   public void periodic() {
-      encoderValue = teleClimbMotor.getSensorCollection().getIntegratedSensorPosition();
-    if (++dashboardCounter >= 5)
-      SmartDashboard.putBoolean("Climb Mode", toggleClimbMode);
-    SmartDashboard.putNumber("Climb Encoder", teleClimbMotor.getSensorCollection().getIntegratedSensorPosition());
+    encoderValue = teleClimbMotor.getSensorCollection().getIntegratedSensorPosition();
+    if (++dashboardCounter >= 5) {
 
-    dashboardCounter = 0;
-     
+      SmartDashboard.putBoolean("Climb Mode", toggleClimbMode);
+      SmartDashboard.putNumber("Climb Encoder", teleClimbMotor.getSensorCollection().getIntegratedSensorPosition());
+
+      dashboardCounter = 0;
+    }
   }
 
   public void setTelescopeSpeed(double speed) {
     if (toggleClimbMode && encoderValue < Constants.topTeleClimbLimit && encoderValue > 0) {
       if (encoderValue > Constants.topTeleCimbSlow && speed > 0) {
         teleClimbMotor.set(ControlMode.PercentOutput, speed * Constants.teleClimbSlowModifier);
-      } 
-        else if (encoderValue < Constants.bottomTeleClimbSlow && speed < 0) {
+      } else if (encoderValue < Constants.bottomTeleClimbSlow && speed < 0) {
         teleClimbMotor.set(ControlMode.PercentOutput, speed * Constants.teleClimbSlowModifier);
+      } else {
+        teleClimbMotor.set(ControlMode.PercentOutput, speed);
       }
-        else{
-          teleClimbMotor.set(ControlMode.PercentOutput, speed);
-        }
-      } 
+    }
 
     else {
       teleClimbMotor.set(ControlMode.PercentOutput, 0);
