ad0339c5177e4bb18f6d7376452fed15d154f5af
msonnier <mike@sonnier.name>
2021-10-24 13:40:18 -0500

    Cleaned up TelescopeClimb subsystem

--- src/main/java/frc/robot/subsystems/TelescopeClimb.java
+++ src/main/java/frc/robot/subsystems/TelescopeClimb.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -31,7 +32,9 @@ public class TelescopeClimb extends SubsystemBase {
     teleClimbMotor = new TalonFX(15);
     teleClimbMotor.setNeutralMode(NeutralMode.Brake);
     teleClimbMotor.setInverted(false);
-    teleClimbMotor.getSensorCollection().setIntegratedSensorPosition(0.0, 100);
+    teleClimbMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+    teleClimbMotor.setSelectedSensorPosition(0.0);
+    // teleClimbMotor.getSensorCollection().setIntegratedSensorPosition(0.0, 0);
     teleClimbMotor.setControlFramePeriod(1, 255);
     teleClimbMotor.setControlFramePeriod(4, 255);
     teleClimbMotor.setControlFramePeriod(8, 255);
@@ -45,34 +48,63 @@ public class TelescopeClimb extends SubsystemBase {
 
   @Override
   public void periodic() {
-    encoderValue = teleClimbMotor.getSensorCollection().getIntegratedSensorPosition();
+    encoderValue = getTelescopePosition();
     if (++dashboardCounter >= 5) {
 
       SmartDashboard.putBoolean("Climb Mode", toggleClimbMode);
-      SmartDashboard.putNumber("Climb Encoder", teleClimbMotor.getSensorCollection().getIntegratedSensorPosition());
-
+      // SmartDashboard.putNumber("Climb Encoder", teleClimbMotor.getSensorCollection().getIntegratedSensorPosition());
+      SmartDashboard.putNumber("Climb Encoder", encoderValue);
       dashboardCounter = 0;
     }
   }
 
   public void setTelescopeSpeed(double speed){ 
-    if (climbOverride && toggleClimbMode){
+
+    if (!toggleClimbMode) {
+      // Not in climb mode, so just stop the climber
+      teleClimbMotor.set(ControlMode.PercentOutput, 0);
+      return;
+    }
+
+    // All code from here down only runs if in climb mode
+    if (climbOverride){
+        // Climb mode is on and override button pressed to let climber move no matter what
         teleClimbMotor.set(ControlMode.PercentOutput, speed);
+        return;
       }
 
-    else if (toggleClimbMode && encoderValue < Constants.topTeleClimbLimit && encoderValue > 0) {
+    // All code from here down only runs if in climb mode and not using override
+    if (encoderValue < Constants.topTeleClimbLimit && encoderValue > 0) {
+      // We are within the climb range of 0 to topTeleClimbLimit
       if (encoderValue > Constants.topTeleCimbSlow && speed > 0) {
+        // Nearing the top so slow down if moving up
         teleClimbMotor.set(ControlMode.PercentOutput, speed * Constants.teleClimbSlowModifier);
       } else if (encoderValue < Constants.bottomTeleClimbSlow && speed < 0) {
+        // Nearing the bottom so slow down if moving down
         teleClimbMotor.set(ControlMode.PercentOutput, speed * Constants.teleClimbSlowModifier);
       } else {
+        // We are not near the top or bottom so full speed is OK
         teleClimbMotor.set(ControlMode.PercentOutput, speed);
       }
     }
 
     else {
-      teleClimbMotor.set(ControlMode.PercentOutput, 0);
+      // We are outside the climb range.  Only allow motion in proper direction
+      if (encoderValue <= 0 && speed > 0) {
+        // We are below the "bottom" of climb range but moving up so its OK
+        teleClimbMotor.set(ControlMode.PercentOutput, speed);
+      }
+      else if (encoderValue >= Constants.topTeleClimbLimit && speed < 0) {
+        // We are above the "top" of climb range but moving down so its OK
+        teleClimbMotor.set(ControlMode.PercentOutput, speed);
+      }
+      else {
+        // Outside climb range and not moving towards climb range so stop
+        teleClimbMotor.set(ControlMode.PercentOutput, 0);
+      }
     }
+
+    
   }
 
   public double getTelescopePosition() {
